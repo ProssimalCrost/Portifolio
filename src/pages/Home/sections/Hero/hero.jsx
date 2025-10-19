2c9743965a1b4be75586240601e077171cbf3a6f
ProssimalCrost <theylon.silva@hotmail.com>
2025-10-19 14:41:41 -0300

    Update download link for CV in Hero section

--- src/pages/Home/sections/Hero/hero.jsx
+++ src/pages/Home/sections/Hero/hero.jsx
@@ -3,6 +3,7 @@ import Avatar from "/public/imagens/avatarnb.png"
 import SimCardDownloadIcon from '@mui/icons-material/SimCardDownload';
 import EmailIcon from '@mui/icons-material/Email';
 import { ParallaxProvider } from "react-scroll-parallax";
+import TheylonAugustoDesenvolvedor from '/public/assets/TheylonAugustoDesenvolvedor.pdf'
 
 const Hero = () => {
 
@@ -30,7 +31,7 @@ const Hero = () => {
 
     const handleDownload = () => {
         const link = document.createElement('a')
-        link.href = '/public/assets/TheylonAugustoDesenvolvedor.pdf',
+        link.href = 'TheylonAugustoDesenvolvedor.pdf',
         link.download = 'curriculo.pdf',
         link.click();
     }
